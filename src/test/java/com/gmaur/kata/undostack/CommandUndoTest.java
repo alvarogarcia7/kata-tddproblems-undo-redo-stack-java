691de0df0b2a9c217e59e0545bdb9c6002885cd0
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-03-25 18:27:57 +0100

    refactor: DRY in tests

--- src/test/java/com/gmaur/kata/undostack/CommandUndoTest.java
+++ src/test/java/com/gmaur/kata/undostack/CommandUndoTest.java
@@ -1,30 +1,34 @@
 package com.gmaur.kata.undostack;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class CommandUndoTest {
 
-	@Test
-	public void document_with_a_command_has_undo() {
-		Document document = new Document();
+	private Document document;
+
+	@Before
+	public void setUp() {
+		document = new Document();
 		document.addCommand(new AppendTextCommand("hola"));
-		assertTrue(document.hasUndo());
 	}
 	
 	@Test
-	public void document_after_undo_has_redo() {
-		Document document = new Document();
-		document.addCommand(new AppendTextCommand("hola"));
+	public void document_with_a_command_has_undo() {
+		assertTrue(document.hasUndo());
+	}
+
+	
+	@Test
+	public void document_after_undo_has_redo() {  
 		document.undo();
 		assertTrue(document.hasRedo());
 	}
 	
 	@Test
 	public void document_after_redo_has_undo_again() {
-		Document document = new Document();
-		document.addCommand(new AppendTextCommand("hola"));
 		document.undo();
 		document.redo();
 		assertTrue(document.hasUndo());
