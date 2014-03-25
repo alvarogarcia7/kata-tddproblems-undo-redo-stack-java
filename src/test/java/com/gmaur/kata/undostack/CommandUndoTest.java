0ead30dbfbb6f4017a51cdd52eba5d020746ec47
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-03-25 18:26:21 +0100

    refactor: extract zombie code

--- src/test/java/com/gmaur/kata/undostack/CommandUndoTest.java
+++ src/test/java/com/gmaur/kata/undostack/CommandUndoTest.java
@@ -6,27 +6,12 @@ import org.junit.Test;
 
 public class CommandUndoTest {
 
-//	@Test
-//	public void document_with_no_command_has_no_undo() {
-//		Document document = new Document();
-//		assertFalse(document.hasUndo());
-//	}
-	
 	@Test
 	public void document_with_a_command_has_undo() {
 		Document document = new Document();
 		document.addCommand(new AppendTextCommand("hola"));
 		assertTrue(document.hasUndo());
 	}
-	
-//	@Test
-//	public void document_with_a_command_has_only_one_undo() {
-//		Document document = new Document();
-//		document.addCommand(new AppendTextCommand("hola"));
-//		assertTrue(document.hasUndo());
-//		document.undo();
-//		assertFalse(document.hasUndo());
-//	}
 	
 	@Test
 	public void document_after_undo_has_redo() {
