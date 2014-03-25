3ed1054c7a9fafe9ffbfb7ea2e0948ee85fd690a
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-03-25 18:41:32 +0100

    refactor: reformat code

--- src/test/java/com/gmaur/kata/undostack/CommandUndoTest.java
+++ src/test/java/com/gmaur/kata/undostack/CommandUndoTest.java
@@ -1,9 +1,9 @@
 package com.gmaur.kata.undostack;
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
 
 public class CommandUndoTest {
 
@@ -14,28 +14,28 @@ public class CommandUndoTest {
 		document = new Document();
 		document.addCommand(new Command());
 	}
-	
+
 	@Test
 	public void document_with_a_command_has_undo() {
 		assertTrue(document.hasUndo());
 	}
-	
+
 	@Test
-	public void document_after_undo_has_redo() {  
+	public void document_after_undo_has_redo() {
 		document.undo();
 		assertTrue(document.hasRedo());
 	}
-	
+
 	@Test
 	public void document_after_redo_has_undo_again() {
 		document.undo();
 		document.redo();
 		assertTrue(document.hasUndo());
 	}
-	
+
 	@Test
-	public void get_printing_message(){
-		assertEquals("[1]",document.toString());
+	public void get_printing_message() {
+		assertEquals("[1]", document.toString());
 	}
 
 }
