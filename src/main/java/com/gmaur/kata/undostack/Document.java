1c36dcc49cc6ca93a15f37f7a24ca31fcb06f9ab
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-03-27 11:24:14 +0100

    extract commands to own file

--- src/main/java/com/gmaur/kata/undostack/Document.java
+++ src/main/java/com/gmaur/kata/undostack/Document.java
@@ -1,45 +1,6 @@
 package com.gmaur.kata.undostack;
 
-import java.util.Stack;
-
 public class Document {
-
-	public static class Commands {
-		private Stack<Command> commands;
-
-		public Commands() {
-			this.commands = new Stack<Command>();
-		}
-
-		public Command peek() {
-			return commands.peek();
-		}
-
-		public Command pop() {
-			return commands.pop();
-		}
-
-		/**
-		 * Prefer using {@link #push(Command)} instead of {@link #add(Command)}
-		 */
-		@Deprecated
-		public void add(Command lastCommand) {
-			commands.push(lastCommand);
-		}
-		
-		public void push(Command lastCommand) {
-			commands.push(lastCommand);
-		}
-		
-		public boolean isNotEmpty() {
-			return !commands.isEmpty();
-		}
-
-		@Override
-		public String toString() {
-			return commands.toString();
-		}
-	}
 
 	private Commands undoCommands = new Commands();
 	private Commands redoCommands = new Commands();
